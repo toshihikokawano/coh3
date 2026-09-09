d7d65cb8f456f4b5df5e94ce5d8af1b26ae32c4c
KAWANO Toshihiko <toshihiko.kawano@gmail.com>
2026-09-08 21:41:01 -0600

    sync files with local

--- omplib/scratch.cpp
+++ omplib/scratch.cpp
@@ -8,7 +8,7 @@ unsigned int OMPscratch(double e,int at,int zt,int ai,int zi,Optical *omp)
 {
   unsigned int pf = OMPKoningDelaroche(e,at,zt,ai,zi,omp);
 
-  omp->rc = 1.25;
+  omp->wv1 = 0.0;
 
   return(pf);
 }
